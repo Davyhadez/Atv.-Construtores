53eae5c4e951b1eb2106278307caa3447d4eaa12
David Lago <davidlgk2267@gmail.com>
2025-09-22 18:26:43 -0300

    Add files via upload

---
+++ ContaBancaria.java
@@ -0,0 +1,28 @@
+public class ContaBancaria {
+    private int numeroConta;
+    private double saldo;
+
+    public ContaBancaria(int numeroConta, double saldo) {
+        this.numeroConta = numeroConta;
+        this.saldo = saldo;
+    }
+    public void deposito(double valor) {
+        if (valor > 0) {
+            saldo += valor;
+        } else {
+            System.out.println("Valor de depósito inválido.");
+        }
+    }
+
+    public void sacar(double valor) {
+        if (valor > 0 && valor <= saldo) {
+            saldo -= valor;
+        } else {
+            System.out.println("Saldo insuficiente ou valor inválido.");
+        }
+    }
+
+    public void getSaldo() {
+        System.out.println("Saldo atual: " + saldo);
+    }
+}
