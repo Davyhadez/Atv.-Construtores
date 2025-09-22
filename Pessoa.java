5d49cff0b71b5ebd659b3dfd0ef81f59ad5a88ff
David Lago <davidlgk2267@gmail.com>
2025-09-22 16:03:53 -0300

    Add files via upload

---
+++ Pessoa.java
@@ -0,0 +1,12 @@
+public class Pessoa {
+    String nome;
+    int idade;
+
+    public Pessoa(String nome, int idade){
+        this.nome = nome;
+        this.idade = idade;
+    }
+}
+
+//Crie um construtor que receba os valores dos atributos
+//No main, crie dois objetos Pessoa e imprima os valores.
