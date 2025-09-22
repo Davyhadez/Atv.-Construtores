5e9ba88114f6da67d02a9aa247c02ca2329dca97
David Lago <davidlgk2267@gmail.com>
2025-09-22 18:26:07 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String [] args) {
+        Scanner scanner = new Scanner(System.in);
+        //ATIVIDADE 1
+        Carro carro = new Carro();
         Pessoa p1 = new Pessoa("David", 21);
         Pessoa p2 = new Pessoa("Samara", 27);
 
@@ -7,5 +13,26 @@ public class Main {
         System.out.println("Nome: " + p1.nome + ", Idade: " + p1.idade);
         System.out.println("Nome: " + p2.nome + ", Idade: " + p2.idade);
         System.out.println("=============================");
+        
+        //ATIVIDADE 2
+        Carro newCar = new Carro();
+        newCar.setMarca("Fiat");
+        newCar.setAno(2021);
+        System.out.println("==== Informações do Carro ====");
+        System.out.println("Marca do carro: " + newCar.getMarca());
+        System.out.println("Ano do veículo: " + newCar.getAno());
+
+        //ATIVIDADE 3
+        ContaBancaria conta_01 = new ContaBancaria(123456, 1000.0);
+        System.out.println("==== Dados Bancários ====");
+        conta_01.getSaldo();
+        System.out.println("Depositando R$ 500,00");
+        conta_01.deposito(500.0);
+        conta_01.getSaldo();
+        System.out.println("Sacando R$ 200,00");
+        conta_01.sacar(200.0);
+        conta_01.getSaldo();
+        System.out.println("=========================");
+
     }
 }
